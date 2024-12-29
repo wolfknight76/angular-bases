473e677ecb3c9ecbb94032c3cb33d08e7f8296b6
Roberto Aguilera <caballerolobo76@gmail.com>
2024-12-28 18:24:29 -0600

    Commit inicial

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-heroes-hero',
+    templateUrl: './hero.component.html',
+    styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+
+    public name: string = 'ironman';
+    public age:  number = 45;
+
+    get capitalizedName(): string {
+        return this.name.toUpperCase();
+    }
+
+    getHeroDescription(): string {
+        return `${this.name} is ${this.age} years old`;
+    }
+
+    changeHero(): void {
+        this.name = 'spiderman';
+    }
+
+    changeAge(): void {
+        this.age = 30;
+    }
+
+    resetForm(): void {
+        this.name = 'ironman';
+        this.age = 45;
+    }
+}
---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { HeroComponent } from "./hero/hero.component";
+import { ListComponent } from "./list/list.component";
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    declarations: [
+        HeroComponent,
+        ListComponent
+    ],
+    exports: [
+        HeroComponent,
+        ListComponent
+    ]
+})
+export class HeroesModule {}
